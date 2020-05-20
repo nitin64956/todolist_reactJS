61b946c3caed8caf304472818119b793c44da1fc
nitin64956 <nitin64956@gmail.com>
2020-05-20 14:56:58 +0530

    Updates

---
+++ precache-manifest.6cd7900a0180208c705ca38046febd89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77f56a91eabfb03081a2eafea6a227f",
+    "url": "/todolist_reactJS/index.html"
+  },
+  {
+    "revision": "eeac0cc217a7781a5d84",
+    "url": "/todolist_reactJS/static/css/main.b4977a57.chunk.css"
+  },
+  {
+    "revision": "2503342a5b3bbb9e89a8",
+    "url": "/todolist_reactJS/static/js/2.be0e7db6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist_reactJS/static/js/2.be0e7db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeac0cc217a7781a5d84",
+    "url": "/todolist_reactJS/static/js/main.73f7c93a.chunk.js"
+  },
+  {
+    "revision": "cb2184d44b17f2635830",
+    "url": "/todolist_reactJS/static/js/runtime-main.c67bd396.js"
+  }
+]);
